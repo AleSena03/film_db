c3c55b7345896c0f01c2f7e729d1a5cff5eaea90
AleSena03 <sena.alessandro03@gmail.com>
2025-02-19 23:24:18 +0100

    Aggiunta di commenti, file README e nuova directory utils dentro src

---
+++ README.md
@@ -0,0 +1,3 @@
+# FILM DB
+Importa tutti i dati di ogni film, da un file Excel, a un database.
+
--- src/utils/logger.py
+++ src/utils/logger.py
@@ -6,38 +6,38 @@ from pathlib import Path
 from typing import Optional
 
 
-# PERCORSO DEL PROGETTO (es. film_db/...)
-PERCORSO_PROGETTO: Path = Path(__file__).resolve().parent.parent
-PERCORSO_CARTELLA_LOG: Path = PERCORSO_PROGETTO / "logs"
-PERCORSO_FILE_LOG: Path = PERCORSO_CARTELLA_LOG / "importa_log.log"
+PATH_PROGETTO: Path = Path(__file__).resolve().parent.parent.parent
+# output -> C:\Users\...\film_db
+PATH_CARTELLA_LOG: Path = PATH_PROGETTO / "logs"
+PATH_FILE_LOG: Path = PATH_CARTELLA_LOG / "importa_log.log"
 
 
 def configura_logger(
         nome_file: Optional[Path] = None,
         bytes_max: int = 1048576,
-        file_backup: int = 5
+        files_backup: int = 5
 ) -> Logger:
     """
-    Configura un logger.
+    Configura un logger con RotatingFileHandler e StreamHandler.
 
     Parameters
     ----------
-    nome_file : Optional[str] = None
+    nome_file : Optional[Path] = None
         Percorso del file di log.
     bytes_max : int = 1048576 (1 MB)
-        Dimensione massima del file.
-    file_backup : int = 5
-        Numero massimo di file di log da mantenere.
+        Dimensione massima del file di log prima della rotazione.
+    files_backup : int = 5
+        Numero di file backup da mantenere.
 
     Returns
     -------
     Logger
-        Logger configurato.
+        Logger configurato per output su file e stream.
     """
     log: Logger = logging.getLogger(__name__)
     log.setLevel(INFO)
 
-    # EVITA ACCUMULO DI HANDLER
+    # EVITA AGGIUNTA DI HANDLER GIA PRESENTI
     if not log.handlers:
         formatter: Formatter = Formatter(
             "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -45,16 +45,16 @@ def configura_logger(
         )
 
         if nome_file:
-            # CREA DIRECTORY E NORMALIZZA PERCORSO DEL FILE
-            percorso_completo: Path = PERCORSO_PROGETTO / nome_file
-            percorso_completo.parent.mkdir(parents=True, exist_ok=True)
+            # CREA DIRECTORY PER IL FILE DI LOG
+            path_assoluto: Path = PATH_PROGETTO / nome_file
+            path_assoluto.parent.mkdir(parents=True, exist_ok=True)
 
             # CREA ROTATING FILE HANDLER
             file_handler: RotatingFileHandler = RotatingFileHandler(
-                filename=percorso_completo,
+                filename=path_assoluto,
                 mode="a",
                 maxBytes=bytes_max,
-                backupCount=file_backup,
+                backupCount=files_backup,
                 encoding="utf-8"
             )
             file_handler.setFormatter(formatter)
@@ -70,7 +70,6 @@ def configura_logger(
 
 # CONFIGURAZIONE DEL LOGGER PRINCIPALE
 logger: Logger = configura_logger(
-    nome_file=PERCORSO_FILE_LOG.relative_to(PERCORSO_PROGETTO),
-    bytes_max=1048576,
-    file_backup=5
+    nome_file=PATH_FILE_LOG.relative_to(PATH_PROGETTO),
+    # output -> logs/importa_log.log
 )
